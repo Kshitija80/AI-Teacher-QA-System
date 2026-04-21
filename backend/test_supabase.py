62c8a8046d0993bc449df15375ff0d6a202e5098
Kshitija More <2023bit019@sggs.ac.in>
2026-04-21 20:14:23 +0530

    final deployment fix

--- backend/test_supabase.py
+++ backend/test_supabase.py
@@ -127,7 +127,6 @@ def clear_history():
 # ==============================
 
 if __name__ == "__main__":
-    webbrowser.open("http://127.0.0.1:5000")
-    #app.run(debug=True)
     import os
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
